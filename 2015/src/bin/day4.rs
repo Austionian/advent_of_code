1e661dc55b5a526b52215d37b8ca73b35371a6e1
Austin Rooks <Austionian@users.noreply.github.com>
2023-12-06 22:05:19 -0600

    explicit parts

--- 2015/src/bin/day4.rs
+++ 2015/src/bin/day4.rs
@@ -1,11 +1,28 @@
-fn main() {
+fn part_one() -> usize {
     let mut res = 0;
     loop {
         let digest = md5::compute(format!("bgvyzdsv{res}"));
-        if &digest[0..3] == &[0x00, 0x00, 0x00] {
-            println!("{res}");
+        if &digest[0..2] == &[0x00, 0x00] && &digest[2] <= &0x0F {
             break;
         }
         res += 1;
     }
+    res
+}
+
+fn part_two() -> usize {
+    let mut res = 0;
+    loop {
+        let digest = md5::compute(format!("bgvyzdsv{res}"));
+        if &digest[0..3] == &[0x00, 0x00, 0x00] {
+            break;
+        }
+        res += 1;
+    }
+    res
+}
+
+fn main() {
+    println!("Part One: {}", part_one());
+    println!("Part Two: {}", part_two());
 }
