3bbdada6d06c9e1a26d27918aa05a74c952309e3
Austin Rooks <Austionian@users.noreply.github.com>
2023-12-16 22:57:06 -0600

    day12

--- 2023/src/bin/day12.rs
+++ 2023/src/bin/day12.rs
@@ -1,45 +1,73 @@
-fn is_valid(springs: &str, groups: &Vec<usize>) -> bool {
-    let mut current = 0;
-    let mut seen = Vec::new();
-    for c in springs.chars() {
-        if c == '.' {
-            if current > 0 {
-                seen.push(current);
-            }
-            current = 0;
-        } else if c == '#' {
-            current += 1;
-        } else {
-            return false;
-        }
-    }
-    if current > 0 {
-        seen.push(current);
-    }
+use std::collections::HashMap;
 
-    seen.iter()
-        .zip(groups)
-        .fold(true, |acc, (a, b)| acc && a == b)
-        && seen.len() == groups.len()
-}
-
-fn find_valid(springs: &str, groups: &Vec<usize>, i: usize) -> usize {
-    if i == springs.len() {
-        if is_valid(springs, groups) {
+fn find_valid(
+    springs: &str,
+    groups: &Vec<usize>,
+    index: usize,
+    group_index: usize,
+    length_of_current_hashes: usize,
+    cache: &mut HashMap<(usize, usize, usize), usize>,
+) -> usize {
+    if let Some(b) = cache.get(&(index, group_index, length_of_current_hashes)) {
+        return *b;
+    }
+    if index == springs.len() {
+        if group_index == groups.len() && length_of_current_hashes == 0 {
             return 1;
+        } else if group_index == groups.len() - 1 && groups[group_index] == length_of_current_hashes
+        {
+            return 1;
+        } else {
+            return 0;
         }
-        return 0;
     }
-    if springs.chars().nth(i) == Some('?') {
-        let new_str = springs[..i].to_string() + "#" + &springs[i + 1..].to_string();
-        let other_str = springs[..i].to_string() + "." + &springs[i + 1..].to_string();
-        return find_valid(&new_str, groups, i + 1) + find_valid(&other_str, groups, i + 1);
+
+    let mut res = 0;
+    for c in ['.', '#'] {
+        if springs.chars().nth(index) == Some(c) || springs.chars().nth(index) == Some('?') {
+            if c == '.' && length_of_current_hashes == 0 {
+                res += find_valid(springs, groups, index + 1, group_index, 0, cache);
+            } else if c == '.'
+                && length_of_current_hashes > 0
+                && group_index < groups.len()
+                && groups[group_index] == length_of_current_hashes
+            {
+                res += find_valid(springs, groups, index + 1, group_index + 1, 0, cache);
+            } else if c == '#' {
+                res += find_valid(
+                    springs,
+                    groups,
+                    index + 1,
+                    group_index,
+                    length_of_current_hashes + 1,
+                    cache,
+                );
+            }
+        }
     }
-    return find_valid(springs, groups, i + 1);
+
+    cache.insert((index, group_index, length_of_current_hashes), res);
+    res
 }
 
-fn main() {
-    let res = include_str!("../../data/twelve.input")
+fn part_one() -> usize {
+    include_str!("../../data/twelve.input")
+        .lines()
+        .fold(0, |acc, line| {
+            let (springs, groups) = line.split_once(' ').unwrap();
+            let groups = groups
+                .split(',')
+                .filter_map(|v| v.parse::<usize>().ok())
+                .collect::<Vec<_>>();
+            let mut cache = HashMap::new();
+
+            let res = find_valid(&springs, &groups, 0, 0, 0, &mut cache);
+            acc + res as usize
+        })
+}
+
+fn part_two() -> usize {
+    include_str!("../../data/twelve.input")
         .lines()
         .fold(0, |acc, line| {
             let (springs, groups) = line.split_once(' ').unwrap();
@@ -48,9 +76,29 @@ fn main() {
                 .filter_map(|v| v.parse::<usize>().ok())
                 .collect::<Vec<_>>();
 
-            let res = find_valid(&springs, &groups, 0);
-            acc + res
-        });
+            let groups = groups
+                .iter()
+                .cycle()
+                .take(5 * groups.len())
+                .map(|v| v.clone())
+                .collect::<Vec<_>>();
 
-    println!("Part One: {res}");
+            let springs = vec![springs]
+                .iter()
+                .cycle()
+                .take(5)
+                .map(|v| v.to_string())
+                .collect::<Vec<_>>()
+                .join("?");
+
+            let mut cache = HashMap::new();
+
+            let res = find_valid(&springs, &groups, 0, 0, 0, &mut cache) as usize;
+            acc + res
+        })
+}
+
+fn main() {
+    println!("Part One: {}", part_one());
+    println!("Part Two: {}", part_two());
 }
