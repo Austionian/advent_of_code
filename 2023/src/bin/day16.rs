7e9b6a8f10e3afef3569efd3ac1e51a6a3db1c61
Austin Rooks <Austionian@users.noreply.github.com>
2023-12-16 21:12:40 -0600

    day16 clean up

--- 2023/src/bin/day16.rs
+++ 2023/src/bin/day16.rs
@@ -51,17 +51,79 @@ impl Position {
 
         Ok(())
     }
+
+    fn get_dir(&mut self, layout: &Vec<Vec<char>>, splits: &mut Vec<Position>) {
+        match layout[self.y][self.x] {
+            '.' => {}
+            '|' => match self.dir {
+                Dir::N | Dir::S => {}
+                Dir::E | Dir::W => {
+                    splits.push(Position {
+                        x: self.x,
+                        y: self.y,
+                        dir: Dir::S,
+                    });
+
+                    self.dir = Dir::N;
+                }
+            },
+            '-' => match self.dir {
+                Dir::E | Dir::W => {}
+                Dir::N | Dir::S => {
+                    splits.push(Position {
+                        x: self.x,
+                        y: self.y,
+                        dir: Dir::W,
+                    });
+
+                    self.dir = Dir::E;
+                }
+            },
+            '\\' => match self.dir {
+                Dir::E => self.dir = Dir::S,
+                Dir::W => self.dir = Dir::N,
+                Dir::S => self.dir = Dir::E,
+                Dir::N => self.dir = Dir::W,
+            },
+            '/' => match self.dir {
+                Dir::E => self.dir = Dir::N,
+                Dir::W => self.dir = Dir::S,
+                Dir::S => self.dir = Dir::W,
+                Dir::N => self.dir = Dir::E,
+            },
+            _ => unreachable!("Invalid layout piece found!"),
+        };
+    }
 }
 
-fn main() -> Result<()> {
-    let layout = include_str!("../../data/sixteen.input")
-        .lines()
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+fn part_one(layout: &Vec<Vec<char>>, height: usize, width: usize) -> Result<usize> {
+    let mut splits = vec![Position {
+        x: 0,
+        y: 0,
+        dir: Dir::E,
+    }];
+    let mut seen = HashSet::new();
+    let mut split_starts = HashSet::new();
+    while splits.len() > 0 {
+        let mut position = splits.pop().unwrap();
+        if !split_starts.insert(position.clone()) {
+            continue;
+        }
+        loop {
+            seen.insert((position.x, position.y));
 
-    let height = layout.len();
-    let width = layout[0].len();
+            position.get_dir(&layout, &mut splits);
 
+            if let Err(_) = position.get_next(height, width) {
+                break;
+            };
+        }
+    }
+
+    Ok(seen.len())
+}
+
+fn part_two(layout: &Vec<Vec<char>>, height: usize, width: usize) -> Result<usize> {
     let mut max = 0;
 
     for x in 0..width {
@@ -79,46 +141,8 @@ fn main() -> Result<()> {
             }
             loop {
                 seen.insert((position.x, position.y));
-                match layout[position.y][position.x] {
-                    '.' => {}
-                    '|' => match position.dir {
-                        Dir::N | Dir::S => {}
-                        Dir::E | Dir::W => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::S,
-                            });
 
-                            position.dir = Dir::N;
-                        }
-                    },
-                    '-' => match position.dir {
-                        Dir::E | Dir::W => {}
-                        Dir::N | Dir::S => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::W,
-                            });
-
-                            position.dir = Dir::E;
-                        }
-                    },
-                    '\\' => match position.dir {
-                        Dir::E => position.dir = Dir::S,
-                        Dir::W => position.dir = Dir::N,
-                        Dir::S => position.dir = Dir::E,
-                        Dir::N => position.dir = Dir::W,
-                    },
-                    '/' => match position.dir {
-                        Dir::E => position.dir = Dir::N,
-                        Dir::W => position.dir = Dir::S,
-                        Dir::S => position.dir = Dir::W,
-                        Dir::N => position.dir = Dir::E,
-                    },
-                    _ => unreachable!("Invalid layout piece found!"),
-                };
+                position.get_dir(layout, &mut splits);
 
                 if let Err(_) = position.get_next(height, width) {
                     break;
@@ -146,46 +170,8 @@ fn main() -> Result<()> {
             }
             loop {
                 seen.insert((position.x, position.y));
-                match layout[position.y][position.x] {
-                    '.' => {}
-                    '|' => match position.dir {
-                        Dir::N | Dir::S => {}
-                        Dir::E | Dir::W => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::S,
-                            });
 
-                            position.dir = Dir::N;
-                        }
-                    },
-                    '-' => match position.dir {
-                        Dir::E | Dir::W => {}
-                        Dir::N | Dir::S => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::W,
-                            });
-
-                            position.dir = Dir::E;
-                        }
-                    },
-                    '\\' => match position.dir {
-                        Dir::E => position.dir = Dir::S,
-                        Dir::W => position.dir = Dir::N,
-                        Dir::S => position.dir = Dir::E,
-                        Dir::N => position.dir = Dir::W,
-                    },
-                    '/' => match position.dir {
-                        Dir::E => position.dir = Dir::N,
-                        Dir::W => position.dir = Dir::S,
-                        Dir::S => position.dir = Dir::W,
-                        Dir::N => position.dir = Dir::E,
-                    },
-                    _ => unreachable!("Invalid layout piece found!"),
-                };
+                position.get_dir(layout, &mut splits);
 
                 if let Err(_) = position.get_next(height, width) {
                     break;
@@ -213,46 +199,8 @@ fn main() -> Result<()> {
             }
             loop {
                 seen.insert((position.x, position.y));
-                match layout[position.y][position.x] {
-                    '.' => {}
-                    '|' => match position.dir {
-                        Dir::N | Dir::S => {}
-                        Dir::E | Dir::W => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::S,
-                            });
 
-                            position.dir = Dir::N;
-                        }
-                    },
-                    '-' => match position.dir {
-                        Dir::E | Dir::W => {}
-                        Dir::N | Dir::S => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::W,
-                            });
-
-                            position.dir = Dir::E;
-                        }
-                    },
-                    '\\' => match position.dir {
-                        Dir::E => position.dir = Dir::S,
-                        Dir::W => position.dir = Dir::N,
-                        Dir::S => position.dir = Dir::E,
-                        Dir::N => position.dir = Dir::W,
-                    },
-                    '/' => match position.dir {
-                        Dir::E => position.dir = Dir::N,
-                        Dir::W => position.dir = Dir::S,
-                        Dir::S => position.dir = Dir::W,
-                        Dir::N => position.dir = Dir::E,
-                    },
-                    _ => unreachable!("Invalid layout piece found!"),
-                };
+                position.get_dir(layout, &mut splits);
 
                 if let Err(_) = position.get_next(height, width) {
                     break;
@@ -280,46 +228,8 @@ fn main() -> Result<()> {
             }
             loop {
                 seen.insert((position.x, position.y));
-                match layout[position.y][position.x] {
-                    '.' => {}
-                    '|' => match position.dir {
-                        Dir::N | Dir::S => {}
-                        Dir::E | Dir::W => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::S,
-                            });
 
-                            position.dir = Dir::N;
-                        }
-                    },
-                    '-' => match position.dir {
-                        Dir::E | Dir::W => {}
-                        Dir::N | Dir::S => {
-                            splits.push(Position {
-                                x: position.x,
-                                y: position.y,
-                                dir: Dir::W,
-                            });
-
-                            position.dir = Dir::E;
-                        }
-                    },
-                    '\\' => match position.dir {
-                        Dir::E => position.dir = Dir::S,
-                        Dir::W => position.dir = Dir::N,
-                        Dir::S => position.dir = Dir::E,
-                        Dir::N => position.dir = Dir::W,
-                    },
-                    '/' => match position.dir {
-                        Dir::E => position.dir = Dir::N,
-                        Dir::W => position.dir = Dir::S,
-                        Dir::S => position.dir = Dir::W,
-                        Dir::N => position.dir = Dir::E,
-                    },
-                    _ => unreachable!("Invalid layout piece found!"),
-                };
+                position.get_dir(layout, &mut splits);
 
                 if let Err(_) = position.get_next(height, width) {
                     break;
@@ -332,7 +242,20 @@ fn main() -> Result<()> {
         }
     }
 
-    println!("{max}");
+    Ok(max)
+}
+
+fn main() -> Result<()> {
+    let layout = include_str!("../../data/sixteen.input")
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let height = layout.len();
+    let width = layout[0].len();
+
+    println!("Part One: {}", part_one(&layout, height, width)?);
+    println!("Part Two: {}", part_two(&layout, height, width)?);
 
     Ok(())
 }
